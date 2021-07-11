4fca2086da9d266a38a9ed5363e4d9fee45176a1
kbz <74146882+kbz0@users.noreply.github.com>
2021-07-11 13:00:36 +0100

    added events to game code
    - added basic event example
    - fixed some narrowing conversion issue with ranges (commented out for now as works fine without it)

--- Game/src/main.cpp
+++ Game/src/main.cpp
@@ -1,9 +1,25 @@
 #include <memory>
+#include <iostream>
 
 #include "game/framework.h"
+#include "engine/events/events.hpp"
+
+struct X : Event_emitter<int, double> {
+	void emit() {
+		post_event(5);
+		post_event(1.0);
+	}
+};
 
 int main(int argc, char* argv[])
 {
 	GameFramework framework;
+
+
+	X x;
+	auto h1 = x.add_callback([](int) -> void { std::cout << "Received int event\n"; });
+	auto h2 = x.add_callback([](double) { std::cout << "Received double event\n"; });
+
+	x.emit();
 
 }
