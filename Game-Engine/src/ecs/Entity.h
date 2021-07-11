b459b3307cab7f88c1d3a8d021958eeb5cff4b75
Rack76 <52178931+Rack76@users.noreply.github.com>
2021-07-11 13:57:56 +0200

    edited entity

--- Game-Engine/src/ecs/Entity.h
+++ Game-Engine/src/ecs/Entity.h
@@ -2,6 +2,8 @@
 
 #include <vector>
 #include <map>
+#include <unordered_map>
+#include <functional>
 #include "Component.h"
 #include "BitMask.h"
 
@@ -9,5 +11,6 @@ using EntityId = unsigned int;
 
 using ComponentContainer = std::vector<Component*>;
 using EntityData = std::pair<BitMask, ComponentContainer>;
-using EntityContainer = std::map<EntityId, EntityData>;
+using EntityContainer = std::unordered_map<EntityId, EntityData>;
 
+using ComponentFactory = std::unordered_map<ComponentType, std::function<Component* (void)>>;
