c99acc6d80fa00846aa3d7e4ff5770d7cb2f1788
Rack76 <52178931+Rack76@users.noreply.github.com>
2021-07-12 19:44:20 +0200

    finished System class

--- Game-Engine/src/engine/ecs/system.cpp
+++ Game-Engine/src/engine/ecs/system.cpp
@@ -12,33 +12,40 @@ System::~System()
 
 bool System::addEntity(const EntityId& entity)
 {
+	if (hasEntity(entity))
+		return false;
 
+	entityList.emplace_back(entity);
 	return true;
 }
 
 bool System::hasEntity(const EntityId& entity)
 {
-	return false;
+	auto itr = std::find_if(entityList.begin(), entityList.end(),
+		[&entity](unsigned int m_entity) {return m_entity == entity; });
+	return (itr == entityList.end());
 }
 
 bool System::removeEntity(const EntityId& entity)
 {
-	return false;
+	auto itr = std::find_if(entityList.begin(), entityList.end(), 
+		[entity](EntityId& id) {return id == entity; });
+
+	if (itr == entityList.end())
+		return false;
+
+	entityList.erase(itr);
+	return true;
 }
 
 bool System::fitsRequirement(const BitMask& bits)
 {
-	return false;
+	return std::find_if(requiredComponents.begin(), requiredComponents.end(), 
+		[&bits](const BitMask& m_bits) {return bits.getMask() == m_bits.getMask(); }) != requiredComponents.end();
 }
 
 void System::purge()
 {
+	entityList.clear();
 }
 
-void System::update(float dt)
-{
-}
-
-void System::handleEvent(const EntityId& entity, const EntityEvent& event)
-{
-}
